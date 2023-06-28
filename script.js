becd19cf91a2163e36b7e6ab76e2088d136e8bb3
wanjugucode <karobiaanastasia@gmail.com>
2023-06-28 12:02:23 +0300

    style

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 document.querySelector('.menu-toggle').addEventListener('click', function() {
     this.classList.toggle('active');
+    document.querySelector('.burger').classList.toggle('active');
     document.querySelector('.menu').classList.toggle('active');
   });
-  
+
+document.querySelector('.close').addEventListener('click', function() {
+    this.classList.toggle('active')
+});
